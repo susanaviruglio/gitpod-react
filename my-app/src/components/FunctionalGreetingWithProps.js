fcc3d27e3155e46a4c01853625989b474de537f9
Susana Viruglio <susanaviruglio@hotmail.com>
2024-05-26 16:35:24 +0000

    added new functional props parameters

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 
-const FunctionalGreetingWithProps = () => <h1>Hello</h1>;
+const FunctionalGreetingWithProps = (props) => {
+    return <h1>Hello, {props.name}! I see you are {props.age} years old; {props.greeting}</h1>; //add a return statement as our function has multiple lines of code
+}
 
 export default FunctionalGreetingWithProps;
